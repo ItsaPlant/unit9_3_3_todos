63f98fafb70102780da0f050119f00d812b15242
ItsaPlant <krzysiekmichalik6@gmail.com>
2021-09-15 14:50:11 +0200

    :c

---
+++ forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from flask_wtf.recaptcha import validators
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired
+
+class TodoForm(FlaskForm):
+    title = StringField('title', validators=[DataRequired()])
+    description = StringField('description', validators=[DataRequired()])
+    done = StringField('done', validators=[DataRequired()])
